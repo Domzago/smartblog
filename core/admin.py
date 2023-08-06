a70eb245702c803fcbb8dc3396a3c410257015c0
resumeap <spotlyt7@gmail.com>
2023-08-06 04:46:26 +0100

    deploying to ng

---
+++ core/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+
+admin.site.site_header = 'Smart Blog Admin'
+admin.site.site_title = 'Smart Blog Area'
+admin.site.index_title = 'Welcome to Smart Blog'
